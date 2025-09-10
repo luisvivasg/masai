fd8aa40934ebd13b0f6a7bc26c5f11aa27e74bed
Luis Vivas <luisvivas@gmail.com>
2025-09-10 10:35:32 +0200

    Agregando basco para streamlit

--- main_app.py
+++ main_app.py
@@ -1,7 +1,13 @@
 import streamlit as st
 from datetime import datetime, timedelta
 
+
+st.write("Ethnicity relevance")
 inicio_dias = datetime.now() - timedelta(730)
 yesterday = datetime.now() - timedelta(2)
 fecha_inicio_imp, first_date, fecha_inicio = datetime.strftime(inicio_dias, "%d-%B-%Y"), datetime.strftime(inicio_dias, "%Y %b %d"), datetime.strftime(inicio_dias, "%Y-%m-%d")
 fecha_fin_imp, last_date, fecha_fin = datetime.strftime(yesterday, "%d-%B-%Y"), datetime.strftime(yesterday, "%Y %b %d"), datetime.strftime(yesterday, "%Y-%m-%d")
+
+ethnicity = ["Masai", "Zulu", "Igbo"]
+
+country = st.selectbox("Select Ethnicity", ethnicity)
